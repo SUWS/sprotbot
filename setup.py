54c9d11eb72f19a72dcf779d19446cc6c4a74959
Darren Richardson <noreply@mnestis.net>
2017-09-13 21:12:07 +0100

    Increment version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name = "sprotbot",
       description = "A bot for sending out SUWS emails.",
       packages = find_packages(),
       author = "Darren Richardson",
-      version = "1",
+      version = "2",
       package_data = {"sprotbot": ["templates/*"]},
       install_requires = [
             "boto3==1.4.4",
